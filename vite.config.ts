4a255bed2355048814c1b1044a7ffaa5f63a2c3e
sebCzabak <sczabak@gmail.com>
2025-06-16 21:16:48 +0200

    Add github actions for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+const repoName = 'https://github.com/sebCzabak/Awhaus';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${repoName}/`, // <--- WAŻNA ZMIANA DLA GITHUB PAGES
+  build: {
+    outDir: 'dist', // Domyślny katalog wyjściowy
+  }
+});
